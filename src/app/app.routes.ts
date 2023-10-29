6188e72c361810edd14a11b2af703cf00dd3741a
Patrick Navarro <patricknv230101@gmail.com>
2023-10-28 22:03:50 -0500

    new: added nofFound component | login and register pages | tailwindCSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    loadComponent: () => import('./pages/login/login.component').then(component => component.LoginComponent)
+  },
+  {
+    path: 'auth/register',
+    loadComponent: () => import('./pages/register/register.component').then(component => component.RegisterComponent)
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./components/not-found/not-found.component').then(component => component.NotFoundComponent)
+  }
+];
