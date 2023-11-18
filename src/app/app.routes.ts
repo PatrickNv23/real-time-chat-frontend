383b6a34a03ad1e6ce98770226110f5516b2e114
Patrick Navarro <patricknv230101@gmail.com>
2023-11-18 17:02:24 -0500

    Add authentication guards to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard, isLoggedInGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -8,15 +9,18 @@ export const routes: Routes = [
   },
   {
     path: 'auth',
-    loadComponent: () => import('./pages/sign-in/sign-in.component').then(component => component.SignInComponent)
+    loadComponent: () => import('./pages/sign-in/sign-in.component').then(component => component.SignInComponent),
+    canActivate: [isLoggedInGuard]
   },
   {
     path: 'auth/sign-up',
-    loadComponent: () => import('./pages/sign-up/sign-up.component').then(component => component.SignUpComponent)
+    loadComponent: () => import('./pages/sign-up/sign-up.component').then(component => component.SignUpComponent),
+    canActivate: [isLoggedInGuard]
   },
   {
     path: 'home',
-    loadComponent: () => import('./pages/home/home.component').then(component => component.HomeComponent)
+    loadComponent: () => import('./pages/home/home.component').then(component => component.HomeComponent),
+    canActivate: [authGuard]
   },
   {
     path: '**',
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from 'src/app/components/sidebar/sidebar.component';
 import { SidebarContentComponent } from 'src/app/components/sidebar-content/sidebar-content.component';
 import { ChatRoomComponent } from 'src/app/components/chat-room/chat-room.component';
-import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -14,11 +13,7 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class HomeComponent {
 
-  constructor(private authService: AuthService) {
-    this.authService.getCurrentSession().subscribe({
-      next: (response: any) => {
-        console.log(response);
-      }
-    })
+  constructor() {
+
   }
 }
